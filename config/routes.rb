2fd2ea5ea873887b7876f57925c257538f719afa
y-shirado-ga <y_shirado@ga-tech.co.jp>
2018-10-25 16:20:06 +0900

    認証用の下準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  to: 'ajax_test#top'
+  #root  to: 'ajax_test#top'
+  root "welcome#index"
   post  'ajax_test/update', to: 'ajax_test#update', as: 'ajax_test_update'
 end
