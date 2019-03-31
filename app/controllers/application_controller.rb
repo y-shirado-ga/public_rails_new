5e73eb77f9df8ac86af859f2f66dcccdeb71aa7a
y-shirado-ga <y_shirado@ga-tech.co.jp>
2019-03-31 14:52:29 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def authenticate_user!
+    session[:user_return_to] = Rails.env['PATH_INFO']
+    redirect_to user_omniauth_authorize_path(:facebook) unless user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   #root  to: 'ajax_test#top'
   root "welcome#index"
   post  'ajax_test/update', to: 'ajax_test#update', as: 'ajax_test_update'
+  get "home", to: "home#index", as: "user_root"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
